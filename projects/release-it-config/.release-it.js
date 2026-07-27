cef87db1074bf1101a4512631f891103c87e7bcc
KimAlexander <kimalexandr1992@gmail.com>
2026-07-27 20:16:00 +0300

    upd

--- projects/release-it-config/.release-it.js
+++ projects/release-it-config/.release-it.js
@@ -1,7 +1,14 @@
 module.exports = {
   git: {
     commitMessage: "chore(release): v${version}",
+    tagAnnotation: "Release v${version}",
     tagName: "v${version}",
+    commitArgs: "--no-verify",
+    pushArgs: ["--follow-tags"],
+    requireBranch: false,
+    requireCleanWorkingDir: false,
+    requireCommits: false,
+    getLatestTagFromAllRefs: true,
     push: true,
   },
 
